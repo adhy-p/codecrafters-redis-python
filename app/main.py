bfe8d24b0ce050d78f9cb738095650e1720824a3
adhy-p <adhyp02@gmail.com>
2023-05-30 00:34:02 +0800

    refactor redis server to class

--- app/main.py
+++ app/main.py
@@ -2,47 +2,59 @@ import select
 import socket
 
 
-def wrap_resp(message):
-    first = b"+"
-    end = b"\r\n"
-    return first + message.encode("utf-8") + end
+class RedisServer:
+    def __init__(self):
+        print("Initializing Redis Server")
+        self.server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+        self.server_socket.setblocking(0)
+        self.active_connections = [self.server_socket]
+
+    def serve(self):
+        while True:
+            print("polling sockets")
+            readable, writable, exceptional = select.select(self.active_connections, [], [])
+
+            for s in readable:
+                if s is self.server_socket:
+                    self._handle_connection()
+                else:
+                    self._handle_client(s)
+
+            for s in writable:
+                pass
+
+            for s in exceptional:
+                self.active_connections.remove(s)
+                s.close()
+
+    def _handle_connection(self):
+        conn, addr = self.server_socket.accept()
+        print(f"received new connection from {addr}")
+        self.active_connections.append(conn)
+        self._handle_client(conn)
+
+    def _handle_client(self, client):
+        data = client.recv(2048)
+        if data:
+            print("sending response to client")
+            client.send(self._wrap_resp("PONG"))
+        else:
+            print("client disconnected")
+            self.active_connections.remove(client)
+            client.close()
+
+    def _wrap_resp(self, message):
+        type_str = b"+"
+        end = b"\r\n"
+        return type_str + message.encode("utf-8") + end
 
 
 def main():
     # You can use print statements as follows for debugging,
     # they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.setblocking(0)
-
-    active_connections = [server_socket]
-
-    while True:
-        print("polling sockets")
-        readable, writable, exceptional = select.select(active_connections, [], [])
-
-        for s in readable:
-            if s is server_socket:
-                conn, addr = server_socket.accept()  # wait for client
-                print(f"received connection from {addr}")
-                active_connections.append(conn)
-                conn.recv(2048)
-                conn.send(wrap_resp("PONG"))
-            else:
-                data = s.recv(2048)
-                if data:
-                    print("sending response to ping message")
-                    s.send(wrap_resp("PONG"))
-                else:
-                    print("client disconnected")
-                    active_connections.remove(s)
-                    s.close()
-
-        for s in exceptional:
-            print("exception")
-            active_connections.remove(s)
-            s.close()
+    # print("Logs from your program will appear here!")
+    rs = RedisServer()
+    rs.serve()
 
 
 if __name__ == "__main__":
