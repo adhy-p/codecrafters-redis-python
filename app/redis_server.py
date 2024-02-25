4d04d5bc5dcfbce5e85d091eadf50f1d9cd8d132
adhy-p <adhyp02@gmail.com>
2024-02-25 14:58:42 +0800

    Pass last stage

--- app/redis_server.py
+++ app/redis_server.py
@@ -1,8 +1,9 @@
 import asyncio
 import logging
+import time
 
 from app.resp_parser import RespParser
-from typing import List
+from typing import List, Tuple
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("redis_server")
@@ -49,24 +50,45 @@ class RedisServer:
             case b"PING":
                 return b"+PONG\r\n"
             case b"ECHO":
+                if len(req) < 1:
+                    return b"-Missing argument(s) for ECHO\r\n"
                 msg: bytes = req[1]
                 msg_len = len(msg)
                 return b"$" + str(msg_len).encode("utf-8") + b"\r\n" + msg + b"\r\n"
             case b"SET":
+                if len(req) < 2:
+                    return b"-Missing argument(s) for SET\r\n"
                 key: bytes = req[1]
                 val: bytes = req[2]
-                self.kvstore[key] = val
+                expiry: int = -1
+                if len(req) > 2:
+                    precision: bytes = req[3]
+                    if precision.upper() != b"PX":
+                        return b"-Currently only supporting PX for SET timeout\r\n"
+                    expiry = time.time_ns() // 1_000_000 + int(req[4].decode())
+                self.kvstore[key] = (val, expiry)
                 return b"+OK\r\n"
             case b"GET":
+                if len(req) < 2:
+                    return b"-Missing argument(s) for GET\r\n"
                 key: bytes = req[1]
-                if key in self.kvstore:
-                    msg: bytes = self.kvstore[key]
-                    msg_len = len(msg)
-                    return b"$" + str(msg_len).encode("utf-8") + b"\r\n" + msg + b"\r\n"
+                self.kvstore.get
+                value: Tuple[bytes, int] | None = self.kvstore.get(key)
+                if value:
+                    msg, expiry = value
+                    if expiry == -1 or time.time_ns() // 1_000_000 <= expiry:
+                        msg_len = len(msg)
+                        return (
+                            b"$"
+                            + str(msg_len).encode("utf-8")
+                            + b"\r\n"
+                            + msg
+                            + b"\r\n"
+                        )
                 return b"-1\r\n"
             case _:
-                logger.error(f"{req[0]} not implemented yet!")
-                return b"+PONG\r\n"
+                logger.error(f"Received {req[0]} command (not supported)!")
+                return b"-Command not supported yet!\r\n"
 
     async def serve(self):
         async with self.server:
