05edf0442f8fe3e5e6b81f7864d4750c97997fb0
adhy-p <adhyp02@gmail.com>
2024-02-25 14:27:18 +0800

    Pass fifth stage

--- app/redis_server.py
+++ app/redis_server.py
@@ -1,6 +1,9 @@
 import asyncio
 import logging
 
+from app.resp_parser import RespParser
+from typing import List
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("redis_server")
 
@@ -15,18 +18,35 @@ class RedisServer:
         logger.info("creating server")
         return self
 
-    @classmethod
     async def connection_handler(
-        cls, reader: asyncio.StreamReader, writer: asyncio.StreamWriter
+        reader: asyncio.StreamReader, writer: asyncio.StreamWriter
     ):
         while True:
             data: bytes = await reader.read(1024)
-            message: str = data.decode()
+            requests = RespParser.parse_request(data)
             addr: str = writer.get_extra_info("peername")
-            logger.info(f"received {message!r} from {addr!r}")
-            writer.write(b"+PONG\r\n")
-            logger.info("replied PONG to client")
-            await writer.drain()
+            if requests:
+                logger.info(f"received {requests!r} from {addr!r}")
+            for req in requests:
+                resp = await RedisServer.handle_request(req)
+                if not resp:
+                    continue
+                writer.write(resp)
+                logger.info(f"replied {resp} to client")
+                await writer.drain()
+
+    async def handle_request(req: List[bytes]) -> bytes:
+        assert len(req) > 0
+        match req[0].upper():
+            case b"PING":
+                return b"+PONG\r\n"
+            case b"ECHO":
+                msg: bytes = req[1]
+                msg_len = len(msg)
+                return b"$" + str(msg_len).encode("utf-8") + b"\r\n" + msg + b"\r\n"
+            case _:
+                logger.error(f"{req[0]} not implemented yet!")
+                return b"+PONG\r\n"
 
     async def serve(self):
         async with self.server:
